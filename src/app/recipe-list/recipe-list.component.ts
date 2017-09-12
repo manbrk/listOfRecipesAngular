9ccb072e32f82641dc565a6272a4dbedcbb71630
manbrk <manbrk@gmail.com>
2017-09-12 16:44:57 +0300

    components on places

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  dateCreated = new Date();
+
+  recipes: Recipe[] = [
+    new Recipe('масло из барбершопа', 'Великолепное масло из барбершопа на Смузийной 7', this.dateCreated),
+    new Recipe('крафтовый борщ', 'Прекрасный борщ от бьюти-влогера', this.dateCreated)
+  ];
+
   constructor() { }
 
   ngOnInit() {
